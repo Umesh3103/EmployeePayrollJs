c424bb8f37c729ed0dc417bddcef6c50816684de
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-19 13:43:00 +0530

    [Umesh]Added: Performing operations to get employee wage according to the given condition

--- EmpWage.js
+++ EmpWage.js
@@ -35,5 +35,53 @@
     }    
     let empWage = calcDailyWage(totalEmpHrs);
     console.log("Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
+
+    let totEmpWage = 0;
+    function sum(dailyWage) {
+        totEmpWage += dailyWage;
+    }
+    empDailyWageArr.forEach(sum);
+    console.log("Emp wage by for each method: "+totEmpWage);
+
+    function totalWages(totalWage, dailyWage) {
+        return totalWage + dailyWage;
+    }
+    console.log("Emp wage with reduce: "+empDailyWageArr.reduce(totalWages,0));
+
+    let dailyCntr = 0;
+    function mapDayWithWage(dailyWage) {
+        dailyCntr++;
+        return dailyCntr+" "+dailyWage;
+    }
+    let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+    console.log(mapDayWithWageArr);
+
+    function fulltimeWage(dailyWage) {
+        return dailyWage.includes("160");
+    }
+    let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage);
+    console.log("Daily wage filter with full time wage earned");
+    console.log(fullDayWageArr);
+
+    function findFulltimeWage(dailyWage) {
+        return dailyWage.includes("160");
+    }
+    console.log("First time fulltime wage was earned on "+mapDayWithWageArr.find(findFulltimeWage));
+
+    function isAllFulltimeWage(dailyWage) {
+        return dailyWage.includes("160");
+    }
+    console.log("check all element have full time wage: "+fullDayWageArr.every(isAllFulltimeWage));
+
+    function isAnyPartTimeWage(dailyWage) {
+        return dailyWage.includes("80");
+    }
+    console.log("Check if any parttime wage: "+mapDayWithWageArr.some(isAnyPartTimeWage));
+
+    function totalDaysWorked(numOfDays, dailyWage) {
+        if(dailyWage > 0) return numOfDays+1;
+        return numOfDays;
+    }
+    console.log("Num of Days Emp Worked: "+ empDailyWageArr.reduce(totalDaysWorked, 0));
 }
 
